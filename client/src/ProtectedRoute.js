bd48402864be8472f5500b0989c29b15dbe8a3fb
Ahnaf <ahnakif257@gmail.com>
2025-05-11 22:09:29 +0600

    Updated files

--- client/src/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-// ProtectedRoute.js
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("fitnest_token");
-  return token ? children : <Navigate to="/authflow" replace />;
-}
--- server/models/Dashboard.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const burntCalDataSchema = new mongoose.Schema({
-  // We store date without time information by setting hours to 0
-  date: { type: Date, required: true, unique: true },
-  calories: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('TrackCal_burnt', burntCalDataSchema);
